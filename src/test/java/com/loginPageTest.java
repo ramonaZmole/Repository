8f02a6a157136aa8d066795537aef11d71a6a6da
ramonaZmole <ramona.zmole@evozon.com>
2015-05-15 14:11:42 +0300

    myRequests check pending and special vacation dates

--- src/test/java/com/loginPageTest.java
+++ src/test/java/com/loginPageTest.java
@@ -16,26 +16,26 @@ import com.steps.myRequestsSteps;
 @RunWith(ThucydidesRunner.class)
 public class loginPageTest {
 
-    @Managed(uniqueSession = true)
-    public WebDriver webdriver;
+	@Managed(uniqueSession = true)
+	public WebDriver webdriver;
 
-//    @ManagedPages(defaultUrl = "http://172.22.4.88:9090/login")
-    public Pages pages;
+	// @ManagedPages(defaultUrl = "http://172.22.4.88:9090/login")
+	public Pages pages;
 
-    @Steps
-    public loginSteps endUser;
-    
-    @Steps
-    public myRequestsSteps myRequestsSteps;
+	@Steps
+	public loginSteps endUser;
 
-    @Issue("#WIKI-1")
-    @Test
-    public void login_with_user_ramonaZmole1() {
-        endUser.is_the_home_page();
+	@Steps
+	public myRequestsSteps myRequestsSteps;
+
+	@Issue("#WIKI-1")
+	@Test
+	public void login_with_user_ramonaZmole1() {
+		endUser.is_the_home_page();
 		endUser.login_With("ramonazmole1", "ramona1");
 		endUser.selectVacation();
 		myRequestsSteps.selectMyRequests();
 		myRequestsSteps.selectApproved();
 		myRequestsSteps.selectApplyButton();
-    }
-} 
+	}
+}
