66a9c6df9fff5897c4d3bd5f6df6a380ffadb435
FlaviusMoldovan <flavius.moldovan@evozon.com>
2015-05-18 14:40:01 +0300

    full NewRequest

--- src/main/java/com/pages/NewVacationRequestPage.java
+++ src/main/java/com/pages/NewVacationRequestPage.java
@@ -1,72 +1,168 @@
 package com.pages;
 
-import ch.lambdaj.function.convert.Converter;
-import net.thucydides.core.annotations.DefaultUrl;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
 
-import java.util.List;
+public class NewVacationRequestPage extends PageObject {
 
-import static ch.lambdaj.Lambda.convert;
+	@FindBy(css = "a[href='http://172.22.4.88:9090/vacation?p_p_id=evovacation_WAR_EvoVacationportlet&amp']")
+	private WebElementFacade NewVacationRequest;
 
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_startDate")
+	private WebElementFacade StartDate;
 
-public class NewVacationRequestPage extends PageObject{
+	@FindBy(css = "table[class='dp_daypicker'] tbody tr:nth-child(4) td:nth-child(3)")
+	private WebElementFacade StartDate12;
 
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_saveButton")
+	private WebElementFacade SaveButton;
+
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_endDate[@name='endDate']")
+	private WebElementFacade EndDate;
+
+	@FindBy(css = "table[class='dp_daypicker'] tbody tr:nth-child(6) td:nth-child(2)")
+	private WebElementFacade EndDate25;
+
+	@FindBy(css = "div#_evovacation_WAR_EvoVacationportlet_newVacationComment.optionalComment")
+	private WebElementFacade CommentButton;
+
+	@FindBy(css = "textarea#_evovacation_WAR_EvoVacationportlet_commentContent")
+	private WebElementFacade CommentField;
 	
-	@FindBy(css="a[href='http://172.22.4.88:9090/vacation?p_p_id=evovacation_WAR_EvoVacationportlet&amp']")
-    private WebElementFacade NewVacationRequest;
-
-    @FindBy(css="input#_evovacation_WAR_EvoVacationportlet_startDate")
-    private WebElementFacade StartDate;
+	@FindBy(css = "div#_evovacation_WAR_EvoVacationportlet_newVacationComment.requiredComment")
+	private WebElementFacade RequiredCommentButton;
 	
-    @FindBy(css="table[class='dp_daypicker'] tbody tr:nth-child(4) td:nth-child(3)")
-    private WebElementFacade StartDate12;
-    
-    @FindBy(css="input#_evovacation_WAR_EvoVacationportlet_saveButton")
-    private WebElementFacade SaveButton;
-    
-    @FindBy(css="name='endDate' + 'input#_evovacation_WAR_EvoVacationportlet_endDate'")
-    private WebElementFacade EndDate;
-    
-    @FindBy(css="table[class='dp_daypicker'] tbody tr:nth-child(6) td:nth-child(2)")
-    private WebElementFacade EndDate25;
-    
-    @FindBy(css="input#_evovacation_WAR_EvoVacationportlet_newVacationComment")
-    private WebElementFacade CommentField;
-    
-        
-	public void SelectNewVacationRequest(){
+	
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_type_CF")
+	private WebElementFacade VacationWithoutPaymentButton;
+
+	@FindBy(name = "duration")
+	private WebElementFacade DurationAndDomainField;
+
+	@FindBy(name = "institution")
+	private WebElementFacade InstitutionNameField;
+
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_type_CM")
+	private WebElementFacade SickLeaveButton;
+
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_type_CS")
+	private WebElementFacade SpecialVacationButton;
+
+	@FindBy(css = " select#_evovacation_WAR_EvoVacationportlet_specialReason")
+	private WebElementFacade SpecialVacationMenuButton;
+
+	@FindBy(css = "select[name='specialReason']")
+	private WebElementFacade specialVacationMarriage;
+
+	@FindBy(css = "select[name='specialReason']")
+	private WebElementFacade specialVacationChildBirth;
+	
+	@FindBy(css = "select[name='specialReason']")
+	private WebElementFacade specialVacationFuneral;
+	
+	@FindBy(css = "select[name='specialReason']")
+	private WebElementFacade specialVacationOther;
+
+	@FindBy(css = "input#_evovacation_WAR_EvoVacationportlet_type_ML")
+	private WebElementFacade maternityLeaveButton;
+	
+	@FindBy(css = "select[name='maternityLeave']")
+	private WebElementFacade maternityLeavePrenatal;
+	
+	@FindBy(css = "select[name='maternityLeave']")
+	private WebElementFacade maternityLeaveConcediu;
+
+	public void SelectNewVacationRequest() {
 		NewVacationRequest.click();
-	    }
-	
-	public void selectStartDate(){
+	}
+
+	public void selectStartDate() {
 		StartDate.click();
-	    }
-	
-	public void selectStartDate12(){
+	}
+
+	public void selectStartDate12() {
 		StartDate12.click();
 	}
-	
-	public void selectSaveButton(){
+
+	public void selectSaveButton() {
 		SaveButton.click();
 	}
-	
-	public void selectEndDate(){
+
+	public void selectEndDate() {
 		StartDate.click();
-	    }
-	
-	public void selectEndDate25(){
+	}
+
+	public void selectEndDate25() {
 		EndDate25.click();
-	    }
+	}
+
+	public void selectCommentButton() {
+		CommentButton.click();
+	}
 	
-	public void selectSComment(){
-		StartDate.click();
-	    }
+	public void selectRequiredCommentButton() {
+		RequiredCommentButton.click();
+	}
+
+	public void selectCommentField() {
+		CommentField.click();
+	}
+
+	public void selectVacationWithoutPaymentButton() {
+		VacationWithoutPaymentButton.click();
+	}
+
+	public void selectDurationAndDomainField() {
+		DurationAndDomainField.click();
+	}
+
+	public void selectInstitutionNameField() {
+		InstitutionNameField.click();
+	}
+
+	public void selectSickLeaveButton() {
+		SickLeaveButton.click();
+	}
+
+	public void selectSpecialVacationButton() {
+		element(SpecialVacationButton).waitUntilVisible();
+		SpecialVacationButton.click();
+	}
+	
+	public void selectSpecialVacationMarriage(String value) {
+		element(specialVacationMarriage).selectByValue(value);
+		waitABit(1000);
+	}
+	
+	public void selectSpecialVacationChildBirth(String value) {
+		element(specialVacationChildBirth).selectByValue(value);
+		waitABit(1000);
+	}
+	
+	public void selectSpecialVacationFuneral(String value) {
+		element(specialVacationFuneral).selectByValue(value);
+		waitABit(1000);
+	}
+	
+	public void selectSpecialVacationOther(String value) {
+		element(specialVacationOther).selectByValue(value);
+		waitABit(1000);
+	}
+	
+	public void selectMaternityLeaveButton() {
+		element(maternityLeaveButton).waitUntilVisible();
+		maternityLeaveButton.click();
+	}
+	
+	public void selectMaternityLeavePrenatal(String value) {
+		element(maternityLeavePrenatal).selectByValue(value);
+		waitABit(1000);
+	}
+	
+	public void selectMaternityLeaveConcediu(String value) {
+		element(maternityLeaveConcediu).selectByValue(value);
+		waitABit(1000);
+	}
+	
 }
--- src/test/java/com/steps/newVacationRequestSteps.java
+++ src/test/java/com/steps/newVacationRequestSteps.java
@@ -6,45 +6,124 @@ import net.thucydides.core.steps.ScenarioSteps;
 import com.pages.LeftMenuPage;
 import com.pages.NewVacationRequestPage;
 
-public class newVacationRequestSteps extends ScenarioSteps{
-LeftMenuPage leftMenu;
-NewVacationRequestPage NewVacationRequest;
+public class newVacationRequestSteps extends ScenarioSteps {
+	LeftMenuPage leftMenu;
+	NewVacationRequestPage NewVacationRequest;
 
-@Step
-public void selectNewVacationRequest(){
-	leftMenu.selectNewVacation();
+	@Step
+	public void selectNewVacationRequest() {
+		leftMenu.selectNewVacation();
+	}
+
+	@Step
+	public void selectStartDate() {
+		NewVacationRequest.selectStartDate();
+		waitABit(3000);
+
+	}
+
+	@Step
+	public void selectStartDate12() {
+		NewVacationRequest.selectStartDate12();
+		waitABit(3000);
+	}
+
+	@Step
+	public void selectEndDate() {
+		NewVacationRequest.selectEndDate();
+		waitABit(3000);
+	}
+
+	@Step
+	public void selectEndDate25() {
+		NewVacationRequest.selectEndDate25();
+		waitABit(3000);
+	}
+
+	@Step
+	public void selectSaveButton() {
+		NewVacationRequest.selectSaveButton();
+		waitABit(7000);
+	}
+
+	@Step
+	public void selectCommentButton() {
+		NewVacationRequest.selectCommentButton();
+	}
+	
+	@Step
+	public void selectRequiredCommentButton() {
+		NewVacationRequest.selectRequiredCommentButton();
+	}
+
+	@Step
+	public void selectCommentField() {
+		NewVacationRequest.selectCommentField();
+		waitABit(3000);
+	}
+
+	/*
+	 * @Step public string writeCommentField(string String){ for(a=0;a<7;a++){
+	 * println("1234567890"); } }
+	 */
+	@Step
+	public void selectVacationWithoutPaymentButton() {
+		NewVacationRequest.selectVacationWithoutPaymentButton();
+	}
+
+	@Step
+	public void selectDurationAndDomainField() {
+		NewVacationRequest.selectDurationAndDomainField();
+	}
+
+	@Step
+	public void selectInstitutionNameField() {
+		NewVacationRequest.selectInstitutionNameField();
+	}
+
+	@Step
+	public void selectSickLeaveButton() {
+		NewVacationRequest.selectSickLeaveButton();
+	}
+
+	@Step
+	public void selectSpecialVacationButton() {
+		NewVacationRequest.selectSpecialVacationButton();
+	}
+
+	
+	@Step
+	public void selectSpecialVacationMarriage(String value) {
+		NewVacationRequest.selectSpecialVacationMarriage(value);
+	}
+	
+	@Step
+	public void selectSpecialVacationChildBirth(String value) {
+		NewVacationRequest.selectSpecialVacationChildBirth(value);
+	}
+	
+	@Step
+	public void selectSpecialVacationFuneral(String value) {
+		NewVacationRequest.selectSpecialVacationFuneral(value);
+	}
+	
+	@Step
+	public void selectSpecialVacationOther(String value) {
+		NewVacationRequest.selectSpecialVacationOther(value);
+	}
+
+	@Step
+	public void selectMaternityLeaveButton() {
+		NewVacationRequest.selectMaternityLeaveButton();
+	}
+	
+	@Step
+	public void selectMaternityLeavePrenatal(String value) {
+		NewVacationRequest.selectMaternityLeavePrenatal(value);
 }
-
-@Step
-public void selectStartDate(){
-	NewVacationRequest.selectStartDate();
-	waitABit(3000);
-
+	
+	@Step
+	public void selectMaternityLeaveConcediu(String value) {
+		NewVacationRequest.selectMaternityLeavePrenatal(value);
 }
-
-@Step
-public void selectStartDate12(){
-	NewVacationRequest.selectStartDate12();
-	waitABit(3000);
-}	
-
-@Step
-public void selectEndDate(){
-	NewVacationRequest.selectEndDate();
-	waitABit(3000);
-}
-
-@Step
-public void selectEndDate25(){
-	NewVacationRequest.selectEndDate25();
-	waitABit(3000);
-}
-
-@Step
-public void selectSaveButton(){
-	NewVacationRequest.selectSaveButton();
-	waitABit(7000);
-	}	
-
-
 }
